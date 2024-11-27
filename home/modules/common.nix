a972d889e7493cad1a18f5699404da4823c3088e
nrew <andrew.sayegh29@gmail.com>
2024-11-26 22:00:35 -0500

    cmon

--- home/modules/common.nix
+++ home/modules/common.nix
@@ -5,11 +5,7 @@
     #../modules/fzf.nix
     #../modules/git.nix
     #../modules/lazygit.nix
-<<<<<<< HEAD
     ../modules/neovim/neovim.nix
-=======
-    ../modules/neovim.nix
->>>>>>> de4332a3299cc6d4ec9fead92d94a5c1821e7f2c
     #../modules/tmux.nix
     #../modules/starship.nix
     ../modules/zsh.nix
