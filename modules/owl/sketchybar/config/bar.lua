4f24d49c51b392a97269aaa4f1c332770e625fa5
nrew <andrew.sayegh29@gmail.com>
2025-05-19 20:14:17 -0400

    feat: Refactor sketchybar configuration and restore init scripts

--- modules/owl/sketchybar/config/bar.lua
+++ modules/owl/sketchybar/config/bar.lua
@@ -2,13 +2,15 @@ local settings = require("config.settings")
 
 sbar.bar({ 																-- Create the bar
 	topmost = "window", 												-- "window" or "screen"
-	height = settings.dimens.graphics.bar.height,						-- Height of the bar
-	color = settings.colors.bar.transparent,							-- Background color of the bar
-	padding_right = settings.dimens.padding.bar,						-- Padding on the right side of the bar
-	padding_left = settings.dimens.padding.bar,							-- Padding on the left side of the bar
-	margin = settings.dimens.padding.bar,								-- Margin of the bar
-	corner_radius = settings.dimens.graphics.background.corner_radius, 	-- Corner radius of the bar
-	y_offset = settings.dimens.graphics.bar.offset,						-- Offset of the bar
+	height = 40,						-- Height of the bar
+	color = settings.colors.with_alpha(settings.colors.bg2, 0.35),							-- Background color of the bar
+	padding_right = 5,						-- Padding on the right side of the bar
+	padding_left = 5,							-- Padding on the left side of the bar
+	blur_radius = 35, 	-- Blur radius of the bar
+	shadow = false,
+	margin = 0,								-- Margin of the bar
+	corner_radius = 15, 	-- Corner radius of the bar
+	y_offset = 0,						-- Offset of the bar
 	-- blur_radius = settings.dimens.graphics.blur_radius, 				-- Blur radius of the bar
-	border_width = 0,													-- Border width of the bar
+	background_color = settings.olors.with_alpha(settings.colors.bar.bg, 0.40),
 })
