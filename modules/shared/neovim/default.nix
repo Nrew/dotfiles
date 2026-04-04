4c7ef39ec042c1b5567865d071a440d4400e6241
Nrew <andrew.sayegh29@gmail.com>
2026-04-04 16:32:52 -0400

    Add programs.neovim home-manager module with LSP extraPackages

---
+++ modules/shared/neovim/default.nix
@@ -0,0 +1,46 @@
+{ pkgs, config, lib, ... }:
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    viAlias = true;
+    extraPackages = with pkgs; [
+      # Lua
+      lua-language-server
+      stylua
+
+      # Nix
+      nixd
+      nixfmt-rfc-style
+
+      # TypeScript / JS
+      typescript-language-server
+      nodejs
+      eslint_d
+      prettierd
+
+      # Python
+      pyright
+      ruff
+      black
+
+      # Rust
+      rust-analyzer
+      rustfmt
+
+      # C / Makefile (clangd)
+      clang-tools
+
+      # Tools required by LazyVim and plugins
+      ripgrep
+      fd
+      lazygit
+      tree-sitter
+    ];
+  };
+
+  xdg.configFile."nvim".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/Users/nrew/.config/dotfiles/modules/shared/neovim/config";
+}
