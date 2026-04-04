38bf5b5aadea55eda099d414ba6baf1e85fd7df8
Nrew <andrew.sayegh29@gmail.com>
2026-04-04 18:05:56 -0400

    Configure conform.nvim formatters (nix-managed, format-on-save)

---
+++ modules/shared/neovim/config/lua/plugins/formatting.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      -- Map filetypes to formatter lists (tried in order, first available wins)
+      formatters_by_ft = {
+        lua             = { "stylua" },
+        nix             = { "nixfmt" },
+        python          = { "ruff_format", "black" },
+        typescript      = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+        javascript      = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        rust            = { "rustfmt" },
+        json            = { "prettierd", "prettier" },
+        jsonc           = { "prettierd", "prettier" },
+        yaml            = { "prettierd", "prettier" },
+        markdown        = { "prettierd", "prettier" },
+        ["markdown.mdx"] = { "prettierd", "prettier" },
+        css             = { "prettierd", "prettier" },
+        html            = { "prettierd", "prettier" },
+      },
+      -- Format on save; fall back to LSP if no formatter is configured
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_fallback = true,
+      },
+    },
+  },
+}
