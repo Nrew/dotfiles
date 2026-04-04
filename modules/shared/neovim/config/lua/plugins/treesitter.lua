958c040e416527a4586d29f4be531527a439aecc
Nrew <andrew.sayegh29@gmail.com>
2026-04-04 18:06:09 -0400

    Add explicit treesitter parser list for all languages

---
+++ modules/shared/neovim/config/lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua", "luadoc", "luap",
+        "nix",
+        "typescript", "tsx", "javascript", "jsdoc",
+        "python",
+        "rust",
+        "c",
+        "json", "json5", "jsonc",
+        "yaml",
+        "toml",
+        "markdown", "markdown_inline",
+        "bash",
+        "vim", "vimdoc",
+        "regex",
+        "query",
+        "html", "css",
+        "diff",
+        "git_config", "git_rebase", "gitcommit", "gitignore",
+      },
+    },
+  },
+}
