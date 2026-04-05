18be198407931130979011015b67ae6dded13b05
Nrew <andrew.sayegh29@gmail.com>
2026-04-04 22:12:12 -0400

    Strip btop to essential settings only

--- modules/shared/btop/default.nix
+++ modules/shared/btop/default.nix
@@ -1,17 +1,13 @@
-{ config, pkgs, lib, ... }:
+{ }:
 
 {
   catppuccin.btop.enable = true;
   programs.btop = {
     enable = true;
-    
     settings = {
-      theme_background = false;
       update_ms = 1000;
       proc_sorting = "cpu lazy";
       proc_tree = true;
-      rounded_corners = true;
-      shown_boxes = "cpu mem net proc";
       vim_keys = true;
     };
   };
